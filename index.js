80a550bb7d893f7b543fb69fc6290826a04273a8
Metoki Hidenori <metoki@nn.iij4u.or.jp>
2023-12-24 23:04:56 +0900

    test

---
+++ index.js
@@ -0,0 +1,27 @@
+// Initialize and add the map
+let map;
+
+async function initMap() {
+  // The location of Uluru
+  const position = { lat: 35.7324903, lng: 139.6340467 };
+  // Request needed libraries.
+  //@ts-ignore
+  const { Map } = await google.maps.importLibrary("maps");
+  const { AdvancedMarkerView } = await google.maps.importLibrary("marker");
+
+  // The map, centered at Uluru
+  map = new Map(document.getElementById("map"), {
+    zoom: 14,
+    center: position,
+    mapId: "DEMO_MAP_ID",
+  });
+
+  // The marker, positioned at Uluru
+  const marker = new AdvancedMarkerView({
+    map: map,
+    position: position,
+    title: "Tokyo,Japan",
+  });
+}
+
+initMap();
